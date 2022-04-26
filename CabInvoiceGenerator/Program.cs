66185604403e32a1d8201861bd30e86d7eb45080
Snehal1499 <snehalpatil1718@gmail.com>
2022-04-26 22:21:27 +0530

    Added Cab Invoice Generator TDD

--- CabInvoiceGenerator/Program.cs
+++ CabInvoiceGenerator/Program.cs
@@ -6,7 +6,15 @@ namespace CabInvoiceGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Cab Invoice Generator Problem");
+            Console.WriteLine("Welcome to Cab InVoice Generator Problem");
+            InvoiceGenerator invoice = new InvoiceGenerator(RideType.NORMAL);
+            double fare = invoice.CalculatingFare(2.0, 5);
+            //UC 5
+            InvoiceGenerator invoice2 = new InvoiceGenerator(RideType.PREMIUM);
+            double fare2 = invoice2.CalculatingFare(2.0, 5);
+            Console.WriteLine($"Normal Ride Fare : {fare}");
+            Console.WriteLine($"Premium Ride Fare : {fare2}");
+
         }
     }
 }
